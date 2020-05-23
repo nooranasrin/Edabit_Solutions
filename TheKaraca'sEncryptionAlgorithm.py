f4cb2612a28d24de7035e8e9268d8f80ffa67e49
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-05-23 12:17:34 +0530

    modified karaca's algorithm

--- TheKaraca'sEncryptionAlgorithm.py
+++ TheKaraca'sEncryptionAlgorithm.py
@@ -1,10 +1,7 @@
 def encrypt(string):
-    alternatives = {'a': 0, 'e': 1, 'o': 2, 'u': 3}
     reverse = string[::-1]
-    new_string = ''
-    for character in reverse:
-        new_string += f'{alternatives.get(character, character)}'
-    return new_string + 'aca'
+    new_string = reverse.maketrans('aeou', '0123')
+    return reverse.translate(new_string) + 'aca'
 
 
 print(encrypt('apple'))
