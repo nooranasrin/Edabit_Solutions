66a52dc95019c99238ce6f8e8257d47787f0a882
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-05-23 13:25:11 +0530

    added sum_dig_prod

---
+++ productOfDigitsOfSum.py
@@ -0,0 +1,18 @@
+# Create a function that takes numbers as arguments, adds them together,
+# and returns the product of digits until the answer is only 1 digit long.
+
+from functools import reduce
+import operator
+
+
+def sum_dig_prod(*args):
+    product = sum(args)
+    while product > 9:
+        product = reduce(operator.mul, [int(num) for num in str(product)])
+    return product
+
+
+print(sum_dig_prod(16, 28))
+print(sum_dig_prod(0))
+print(sum_dig_prod(1, 2, 3, 4, 5, 6))
+
